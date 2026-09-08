84f515ed22c0b7eb78d42a419001c55fe1e85d25
Cyrille31 <cyrille.gindre.31@gmail.com>
2026-09-08 17:25:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mc-cgexcel-v29';
+const CACHE = 'mc-cgexcel-v30';
 const FICHIERS = ['./', './index.html', './moteur.js', './worker.js',
                   './lexique.txt', './manifest.webmanifest'];
 
