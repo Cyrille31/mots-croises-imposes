60b856c25c0020d26252e70a2800ec23c37aae4d
Cyrille31 <cyrille.gindre.31@gmail.com>
2026-09-13 08:48:19 +0200

    Add files via upload

--- moteur.js
+++ moteur.js
@@ -1,7 +1,7 @@
 // CGExcel - Generateur de mots croises francais - moteur v4 (JS)
 'use strict';
 
-const VERSION = '6.6';
+const VERSION = '7.0';
 const NOIR = -2, VIDE = -1;
 
 function normaliser(s) {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mc-cgexcel-v46';
+const CACHE = 'mc-cgexcel-v47';
 const FICHIERS = ['./', './index.html', './moteur.js', './worker.js',
                   './lexique.txt', './manifest.webmanifest'];
 
