2c8e00a77df3811e6cc73f7effaa82a07dc7319a
Cyrille31 <cyrille.gindre.31@gmail.com>
2026-09-17 14:26:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CGExcel - Mots croisés - service worker
-const CACHE = 'mc-cgexcel-v68';
+const CACHE = 'mc-cgexcel-v69';
 const FICHIERS = ['./', './index.html', './moteur.js', './worker.js',
                   './lexique.txt', './manifest.webmanifest'];
 
